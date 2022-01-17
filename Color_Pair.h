19f1a5229e5b88c44bf7264a104c8b4e61ce6acd
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-01-17 18:30:53 +0530

    Update Color_Pair.h

--- Color_Pair.h
+++ Color_Pair.h
@@ -13,6 +13,7 @@ const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
+
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
