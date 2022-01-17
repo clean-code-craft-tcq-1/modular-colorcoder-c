b4a50e5ee6621559a100e43860ba759752ee2f6c
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-01-17 20:27:25 +0530

    Update Color_Pair.h

--- Color_Pair.h
+++ Color_Pair.h
@@ -1,5 +1,4 @@
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
 
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
