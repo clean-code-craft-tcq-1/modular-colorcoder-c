d55dc56dc0bd820e4e0f51e9a418c72e2f591a62
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-01-17 20:07:27 +0530

    Update Color_Pair.h

--- Color_Pair.h
+++ Color_Pair.h
@@ -18,6 +18,4 @@ typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
-ColorPair GetColorFromPairNumber(int pairNumber);
-int GetPairNumberFromColor(const ColorPair* colorPair);
+
